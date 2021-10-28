9963a5a076c03c65a144795a2e68e75e0975794f
Gabriella Quintero <gabqui0405@skola.goteborg.se>
2021-10-28 15:49:00 +0200

    Deleted printer

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,6 @@
 public class GuessingGame {
     public static void main(String[] args) {
 	Guesser guesser = new Guesser(0, 1000); 
-	guesser.start();
-	System.out.println(guesser); 
+	guesser.start(); 
     }
 }
